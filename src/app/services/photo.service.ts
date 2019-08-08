35b14dff3fc1d5c9a5654aa493e8e712c1197521
Adam Lovatt <jalovatt@hotmail.com>
2019-08-07 20:56:03 -0600

    Implement a basic gallery

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  public photos: Photo[] = [];
+  constructor(
+    private camera: Camera,
+    public photoService: PhotoService,
+  ) {}
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.photos.unshift({
+        data: 'data:image/jpeg;base64,' + imageData
+      });
+    }, (err) => {
+      console.log('Camera issue:' + err);
+    });
+  }
+}
+
+class Photo {
+  data: any;
+}
